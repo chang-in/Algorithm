7929fbbbdcfcb9e4dc4c3170f9858143600e2970
Changin OH <71659709+chang-in@users.noreply.github.com>
2026-05-16 15:22:07 +0900

    Solve: 두 수 더하기

--- p001/solution.py
+++ p001/solution.py
@@ -1,2 +1,3 @@
-#include <stdio.h>
-int main() { int a, b; scanf("%d %d", &a, &b); printf("%d\n", a+b); return 0; }
+#include <iostream>
+using namespace std;
+int main() { int a, b; cin >> a >> b; cout << a + b << endl; return 0; }
